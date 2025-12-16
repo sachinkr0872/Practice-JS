2ad6bebe09ee761b585bbf54503392478dab4dc6
sachinkr0872 <sachinkr0872@gmail.com>
2025-12-16 16:54:29 +0530

    feat: add concatenate strings and numbers

--- practice.js
+++ practice.js
@@ -1,9 +1,26 @@
-console.log("hello world")
+// console.log("hello world")
 
-let obj={
-    name:"sachin"
+// let obj={
+//     name:"sachin"
 
-}
+// }
 
-obj.name= "sk"
-console.log(obj)
+// obj.name= "sk"
+// console.log(obj)
+
+// alert('hello world')
+
+// console.log('hello world')
+// console.error('this is the error')
+// console.warn('thid is warning')
+
+
+const name = 'sachin';
+const age = 25;
+
+// concatenation
+// console.log(`my name is ${name} and i am ${age}`) ; 
+
+const hello  = `my name is ${name} and i am ${age}`
+
+console.log(hello)

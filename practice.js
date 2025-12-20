58f19b06ee6c726deff38c1b97cc66de256fd30d
sachinkr0872 <sachinkr0872@gmail.com>
2025-12-20 23:48:55 +0530

    add getElementById

--- practice.js
+++ practice.js
@@ -1,26 +1,3 @@
-// console.log("hello world")
-
-// let obj={
-//     name:"sachin"
-
-// }
-
-// obj.name= "sk"
-// console.log(obj)
-
-// alert('hello world')
-
-// console.log('hello world')
-// console.error('this is the error')
-// console.warn('thid is warning')
-
-
-const name = 'sachin';
-const age = 25;
-
-// concatenation
-// console.log(`my name is ${name} and i am ${age}`) ; 
-
-const hello  = `my name is ${name} and i am ${age}`
-
-console.log(hello)
+// #2 getElementById 
+document.getElementById('page-banner')    //by the help of this we can get element by the id name 
+  
